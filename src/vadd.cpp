d3611eb64aef1028c6ff289c7672e76af2abf335
Elias Koromilas <elias.koromilas@gmail.com>
2021-08-13 14:58:48 +0300

    Add vector addition example

---
+++ src/vadd.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <cassert>
+#include <inaccel/coral>
+#include <iostream>
+
+int main() {
+	int size = 1024;
+
+	std::pmr::set_default_resource(inaccel::pmr::resource());
+	std::pmr::vector<float> a(size), b(size), c(size);
+
+	std::pmr::set_default_resource(nullptr);
+	std::pmr::vector<float> c_golden(size);
+
+	std::generate(a.begin(), a.end(), std::rand);
+	std::generate(b.begin(), b.end(), std::rand);
+
+	inaccel::request vadd("vector.addition");
+	vadd.arg(a).arg(b).arg(c).arg(size);
+	inaccel::submit(vadd).get();
+
+	std::transform(a.begin(), a.end(), b.begin(), c_golden.begin(), std::plus());
+
+	assert(c == c_golden);
+
+	std::cout << "Test PASSED" << std::endl;
+}
